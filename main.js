a6b2964f72534022f4bccd86ffadb866f9a3b157
meetelijah <56805137+meetelijah@users.noreply.github.com>
2020-02-01 23:39:27 -0500

    ball and paddle working

--- main.js
+++ main.js
@@ -24,6 +24,9 @@ var paddleHeight = 10;
 var paddleWidth = 75;
 var paddleX = (canvas.width - paddleWidth) / 2;
 var paddleY = canvas.height - paddleHeight;
+//paddle key var
+var rightPressed = false;
+var leftPressed = false;
 
 function drawPaddle() {
     ctx.beginPath();
@@ -37,14 +40,42 @@ function draw() {
     ctx.clearRect(0,0,canvas.width,canvas.height);
     drawBall();
     drawPaddle();
+    //ball movement
     x += dx;
     y += dy;
+    //paddle movement
+    if (rightPressed && paddleX + paddleWidth < canvas.width) {
+        paddleX += 7;
+    } else if (leftPressed && paddleX > 0) {
+        paddleX += -7;
+    }
     //wall collision logic
     if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx;
-    } else if (y + dy < ballRadius || y + dy > canvas.height - ballRadius) {
+    } else if (y + dy < ballRadius || (y + dy > canvas.height - ballRadius && x + dx > paddleX && x + dx < paddleX + paddleWidth)) {
         dy = -dy;
     }
 }
 
+function keyDownHandler(e) {
+    if (e.key == 'Right' || e.key == 'ArrowRight') {
+        rightPressed = true;
+    } else if (e.key == 'Left' || e.key == 'ArrowLeft') {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if (e.key == 'Right' || e.key == 'ArrowRight') {
+        rightPressed = false;
+    } else if (e.key == 'Left' || e.key == 'ArrowLeft') {
+        leftPressed = false;
+    }
+}
+
+
 setInterval(draw, 10);
+
+//event listeners for paddle movement
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
